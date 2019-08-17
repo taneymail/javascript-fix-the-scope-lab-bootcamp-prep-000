3d97fc611cbb61af3094b0bddfae3bb33a0d35d2
TShipley <taneymail@gmail.com>
2019-08-17 04:45:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+var animal = 'cat'
 
+function myAnimal() {
+  return animal
+}
+
+var animal = 'dog'
+
+function yourAnimal() {
+  var animal = 'cat';
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+  return n + two
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+var theFunk = funkyFunction()(); 
